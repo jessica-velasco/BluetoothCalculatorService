648789f8804bd451edf1c9e4abb3a6602f18d51e
jessica-velasco <jessicavelasco12@gmail.com>
2013-07-24 14:00:38 -0700

    Added new features to BluetoothMessenger
    New function can receive a String and solve it

--- src/com/example/nfccalculator/BluetoothMessenger.java
+++ src/com/example/nfccalculator/BluetoothMessenger.java
@@ -3,6 +3,7 @@ package com.example.nfccalculator;
 import android.util.Log;
 
 public class BluetoothMessenger {
+	
 	private MainActivity mainActivity;
 	private String finalString, secondNum, firstNum;
 	
@@ -18,6 +19,27 @@ public class BluetoothMessenger {
 		secondNum=  Integer.valueOf(second).toString();
 		finalString= firstNum + " " + operator + " " + secondNum;
 		Log.i("BluetoothMessenger", finalString);
+		solveString(finalString);
+	}
+	
+	public Integer solveString (String expression){
+		//only works with binary arithmetic expressions i.e. 2+2
+		String[] splitExp;
+		String op="";
+		Integer ans;
+		
+		splitExp= expression.split(" ");
+		ans=Integer.valueOf(splitExp[0]);
+		
+		op=splitExp[1];
+
+		if (op.contentEquals("+"))
+			ans+=Integer.valueOf(splitExp[2]);
+		else if (op.contentEquals("-"))
+			ans-=Integer.valueOf(splitExp[2]);
+			//if (splitExp[i].matches("[0-9]+"))			
+			
+		return ans;
 	}
 
 }
